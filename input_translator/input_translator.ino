5bbf3f0abac941813a4cac5885012eae32d7fb12
Gary Johnson <gary@gjtt.com>
2015-02-02 09:47:17 -0500

    Adding case for input translator board

--- input_translator/input_translator.ino
+++ input_translator/input_translator.ino
@@ -1,9 +1,10 @@
 #include <hidboot.h>
 #include <usbhub.h>
+#include <Encoder.h>
 #include "mouse_handler.h"
 
-int MAX_MOUSE_RANGE = 254;
-int MAX_JOYSTICK_RANGE = 1023;
+int joy_x = 512;
+int joy_right_trigger = 512;
 
 USB usb;
 USBHub hub(&usb);
@@ -11,35 +12,92 @@ HIDBoot<HID_PROTOCOL_MOUSE> hid_mouse(&usb);
 uint32_t next_time;
 MouseHandler mouse_handler;
 
-void setup() {
-  Serial.begin( 115200 );
+Encoder myEnc(12, 13);
+long oldPosition  = -999;
 
-  if (usb.Init() == -1)
-    Serial.println("OSC did not start.");
+void setup() {
+
+  //Serial.begin( 115200 );
+  
+  Joystick.useManualSend(true);
+  
+  pinMode(14, INPUT_PULLUP);
+  pinMode(11, OUTPUT);
+
+  //if (usb.Init() == -1)
+    //Serial.println("OSC did not start.");
 
   delay( 200 );
   next_time = millis() + 5000;
 
+
   hid_mouse.SetReportParser(0,(HIDReportParser*)&mouse_handler);
 }
 
-int loops_without_travel = 0;
-void loop() {
-  if (loops_without_travel > 10000) {
-    loops_without_travel = 0;
+
+void read_encoder() {
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    if (oldPosition > newPosition) {
+      if(joy_right_trigger - 10 >= 0) {
+        joy_right_trigger = joy_right_trigger - 10;
+      }
+    } else {
+      if(joy_right_trigger + 10 <= 1023) {
+       joy_right_trigger = joy_right_trigger + 10;
+      }
+    }
+    oldPosition = newPosition;
+  }
+}
+
+/*
+void process_mouse() {
+  mouse_handler.reset_travel();
+  usb.Task();
+  int y_travel = mouse_handler.get_y_travel();
+  y_travel = y_travel * -1;
+  Mouse.move(y_travel, 0);
+}
+*/
+
+void process_joystick() {
+  usb.Task();
+  
+  int y_travel = mouse_handler.get_y_travel();
+   
+  if (y_travel < 0) {
+    joy_x = 0;
+  }
+  
+  if (y_travel > 0) {
+    joy_x = 1023;
+  }
+  
+  if (mouse_handler.get_checks_without_travel() > 50) {
+    joy_x = 512;
     mouse_handler.reset_travel();
   }
   
-  usb.Task();
-  int y_travel = mouse_handler.get_y_travel();
-  if (y_travel == 0) {
-    loops_without_travel++;
+  Joystick.X(joy_x);
+  Joystick.Y(joy_x);
+  Joystick.sliderRight(joy_right_trigger);
+
+  if(digitalRead(14) == LOW) {
+    digitalWrite(11, HIGH);
+    Joystick.button(1, 1);
+  } else {
+    digitalWrite(11, LOW);
+    Joystick.button(1, 0);
   }
-  
-  unsigned long y_positive = (MAX_MOUSE_RANGE / 2) + y_travel + 1;
-  unsigned long y_translated = (y_positive * 100) / MAX_MOUSE_RANGE;
-  unsigned long joystick_x_translated = y_translated * MAX_JOYSTICK_RANGE;
-  unsigned long joystick_x = joystick_x_translated / 100;
-  Joystick.X(joystick_x);
+    
+  Joystick.send_now();
+}
+
+
+void loop() {
+  //process_mouse();
+  read_encoder();
+  process_joystick();
 }
 
--- input_translator/mouse_handler.h
+++ input_translator/mouse_handler.h
@@ -2,8 +2,10 @@
 
 class MouseHandler : public MouseReportParser { 
   int y_travel;
+  int checks_without_travel;
 public:
   virtual void OnMouseMove(MOUSEINFO *mouse_info);
   void reset_travel();
   int get_y_travel();
+  int get_checks_without_travel();
 };
