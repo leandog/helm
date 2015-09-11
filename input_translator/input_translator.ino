afaba070275d1f3d25e11b1f580ef432b11cce52
Gary Johnson <gary@gjtt.com>
2015-09-10 23:10:19 -0400

    Use new input translator with double foot switch

--- input_translator/input_translator.ino
+++ input_translator/input_translator.ino
@@ -1,10 +1,7 @@
+#include <spi4teensy3.h>
 #include <hidboot.h>
 #include <usbhub.h>
-#include <Encoder.h>
 #include "mouse_handler.h"
-
-int joy_x = 512;
-int joy_right_trigger = 512;
 
 USB usb;
 USBHub hub(&usb);
@@ -12,92 +9,57 @@ HIDBoot<HID_PROTOCOL_MOUSE> hid_mouse(&usb);
 uint32_t next_time;
 MouseHandler mouse_handler;
 
-Encoder myEnc(12, 13);
-long oldPosition  = -999;
-
 void setup() {
+  //Serial.begin(115200);
+  pinMode(15, INPUT);
+  pinMode(16, INPUT);
 
-  //Serial.begin( 115200 );
-  
-  Joystick.useManualSend(true);
-  
-  pinMode(14, INPUT_PULLUP);
-  pinMode(11, OUTPUT);
-
-  //if (usb.Init() == -1)
-    //Serial.println("OSC did not start.");
-
+  usb.Init();
+   //if (usb.Init() == -1)
+       //Serial.println("OSC did not start.");
+       
   delay( 200 );
   next_time = millis() + 5000;
-
 
   hid_mouse.SetReportParser(0,(HIDReportParser*)&mouse_handler);
 }
 
-
-void read_encoder() {
-  long newPosition = myEnc.read();
-  if (newPosition != oldPosition) {
-    if (oldPosition > newPosition) {
-      if(joy_right_trigger - 10 >= 0) {
-        joy_right_trigger = joy_right_trigger - 10;
-      }
-    } else {
-      if(joy_right_trigger + 10 <= 1023) {
-       joy_right_trigger = joy_right_trigger + 10;
-      }
-    }
-    oldPosition = newPosition;
-  }
-}
-
-/*
 void process_mouse() {
   mouse_handler.reset_travel();
   usb.Task();
   int y_travel = mouse_handler.get_y_travel();
-  y_travel = y_travel * -1;
-  Mouse.move(y_travel, 0);
-}
-*/
 
-void process_joystick() {
-  usb.Task();
+  //Serial.println(y_travel);
   
-  int y_travel = mouse_handler.get_y_travel();
-   
-  if (y_travel < 0) {
-    joy_x = 0;
-  }
-  
-  if (y_travel > 0) {
-    joy_x = 1023;
-  }
-  
-  if (mouse_handler.get_checks_without_travel() > 50) {
-    joy_x = 512;
-    mouse_handler.reset_travel();
-  }
-  
-  Joystick.X(joy_x);
-  Joystick.Y(joy_x);
-  Joystick.sliderRight(joy_right_trigger);
-
-  if(digitalRead(14) == LOW) {
-    digitalWrite(11, HIGH);
-    Joystick.button(1, 1);
+  int abs_y_travel = abs(y_travel);
+  if(abs_y_travel > 3) {
+    if(y_travel < 0) {
+      Keyboard.set_key1(KEY_RIGHT);
+    } else {
+      Keyboard.set_key1(KEY_LEFT);
+    }
   } else {
-    digitalWrite(11, LOW);
-    Joystick.button(1, 0);
+    Keyboard.set_key1(0);
   }
-    
-  Joystick.send_now();
+  
+  if(digitalRead(15) == HIGH) {
+    Keyboard.set_modifier(MODIFIERKEY_SHIFT);
+  } else {
+    Keyboard.set_modifier(0);
+  }
+  
+  if(digitalRead(16) == HIGH) {
+    Keyboard.set_key2(KEY_ENTER);
+  } else {
+    Keyboard.set_key2(0);
+  }
+  
+  Keyboard.send_now();
+  
 }
-
 
 void loop() {
-  //process_mouse();
-  read_encoder();
-  process_joystick();
+  process_mouse();
+  delay(10);
 }
 
