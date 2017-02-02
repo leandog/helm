bd78525061e2458a750e35808b34b2d8e005bff2
Gary Johnson <gary@gjtt.com>
2017-02-02 14:38:48 -0500

    Appears as Xinput joystick instead of keyboard

--- input_translator/input_translator.ino
+++ input_translator/input_translator.ino
@@ -1,6 +1,7 @@
 #include <spi4teensy3.h>
 #include <hidboot.h>
 #include <usbhub.h>
+#include <xinput.h>
 #include "mouse_handler.h"
 
 USB usb;
@@ -8,15 +9,14 @@ USBHub hub(&usb);
 HIDBoot<USB_HID_PROTOCOL_MOUSE> hid_mouse(&usb);
 uint32_t next_time;
 MouseHandler mouse_handler;
+XINPUT controller(NO_LED);
 
 void setup() {
-  //Serial.begin(115200);
+  Serial.begin(115200);
   pinMode(15, INPUT);
   pinMode(16, INPUT);
 
   usb.Init();
-   //if (usb.Init() == -1)
-       //Serial.println("OSC did not start.");
        
   delay( 200 );
   next_time = millis() + 5000;
@@ -34,27 +34,38 @@ void process_mouse() {
   int abs_y_travel = abs(y_travel);
   if(abs_y_travel > 3) {
     if(y_travel < 0) {
-      Keyboard.set_key1(KEY_RIGHT);
+      //Keyboard.set_key1(KEY_RIGHT);
+      controller.stickUpdate(STICK_LEFT, 32000, 0);
     } else {
-      Keyboard.set_key1(KEY_LEFT);
+      //Keyboard.set_key1(KEY_LEFT);
+      controller.stickUpdate(STICK_LEFT, -32000, 0);
     }
   } else {
-    Keyboard.set_key1(0);
+    //Keyboard.set_key1(0);
+    controller.stickUpdate(STICK_LEFT, 0, 0);
   }
   
   if(digitalRead(15) == HIGH) {
-    Keyboard.set_modifier(MODIFIERKEY_SHIFT);
+    //Keyboard.set_modifier(MODIFIERKEY_SHIFT);
+    controller.buttonUpdate(BUTTON_A, true);
   } else {
-    Keyboard.set_modifier(0);
+    //Keyboard.set_modifier(0);
+    controller.buttonUpdate(BUTTON_A, false);
   }
   
   if(digitalRead(16) == HIGH) {
-    Keyboard.set_key2(KEY_ENTER);
+    //Keyboard.set_key2(KEY_ENTER);
+    controller.buttonUpdate(BUTTON_B, true);
   } else {
-    Keyboard.set_key2(0);
+    //Keyboard.set_key2(0);
+    controller.buttonUpdate(BUTTON_B, false);
   }
   
-  Keyboard.send_now();
+  //Keyboard.send_now();
+  controller.sendXinput();
+
+  //Receive data
+  controller.receiveXinput();
   
 }
 
